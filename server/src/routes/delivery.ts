7e3de538f06a670274ef8777781ae419ebd96a80
krishnadev <krishnadevkichus@gmail.com>
2025-01-17 19:32:42 +0530

    Add utility functions for distance calculation and integrate Distance Matrix API

--- server/src/routes/delivery.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getDeliveryStations, createOrder } from '../controllers/deliveryController';
-
-const router: Router = Router();
-
-router.get('/stations', getDeliveryStations);
-router.post('/order', createOrder);
-
-export default router;
