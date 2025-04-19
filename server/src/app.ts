32caed43573afa65b1c5a0bdbb30b63b761f765e
kichuu <krishnadevr07@gmail.com>
2025-04-20 00:15:45 +0530

    style: format code for consistency and readability

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,26 +1,27 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import routes from './routes/index';
-import { connectDB } from './config/db';
-import authRoutes from './routes/authRoutes';
+import express, { Application } from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
+import routes from "./routes/index"
+import { connectDB } from "./config/db"
+import authRoutes from "./routes/authRoutes"
 // import { errorHandler } from './middlewares/errorHandler';
 
-const app: Application = express();
+const app: Application = express()
 
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Database Connection
-connectDB();
+connectDB()
+app.set("trust proxy", true)
 
 // Routes
-app.use('/api', routes);
-app.use('/api/auth', authRoutes);
+app.use("/api", routes)
+app.use("/api/auth", authRoutes)
 
 // Error Handling Middleware (if implemented)
 // app.use(errorHandler);
 
-export default app;
+export default app
